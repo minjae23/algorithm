00c0ccbb60c31e46b58ee60a7ac0c1ef79aca5d6
Minjae <010626kmj@gmail.com>
2024-11-25 22:51:48 +0900

    [Bronze V] Title: A+B, Time: 228 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int a,b;
+        Scanner keyboard= new Scanner(System.in);
+        a=keyboard.nextInt();
+        b=keyboard.nextInt();
+        System.out.println(a+b);
+    } 
+}
